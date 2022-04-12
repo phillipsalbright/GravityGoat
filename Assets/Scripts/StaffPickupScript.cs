855fb88104a0fb33e8c915db59f827b7d3b95686
Phillips <phillipsalbright4@gmail.com>
2022-04-12 16:12:40 -0400

    made particles and light fade out when staff picked up

--- Assets/Scripts/StaffPickupScript.cs
+++ Assets/Scripts/StaffPickupScript.cs
@@ -6,6 +6,9 @@ public class StaffPickupScript : MonoBehaviour
 {
     [SerializeField] private GameObject playerWithStaff;
     [SerializeField] private GameObject tutorialText;
+    [SerializeField] private ParticleSystem particles;
+    [SerializeField] private GameObject door;
+    [SerializeField] private GameObject levelLoader;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,9 +16,13 @@ public class StaffPickupScript : MonoBehaviour
         {
             Vector3 pos = other.transform.position;
             this.GetComponent<BoxCollider>().enabled = false;
-            tutorialText.SetActive(true);
+            this.GetComponent<SpriteRenderer>().enabled = false;
             Destroy(other.gameObject);
             Instantiate(playerWithStaff, pos, Quaternion.Euler(Vector3.zero));
+            tutorialText.SetActive(true);
+            door.SetActive(true);
+            levelLoader.SetActive(true);
+            particles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
             Destroy(this.gameObject);
         }
     }
