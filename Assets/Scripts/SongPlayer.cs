7a2ed696820d134343a8ebb95aa9ad4799a21d2c
Phillips <phillipsalbright4@gmail.com>
2022-04-26 21:37:15 -0400

    Added extra level and animation transitions

--- Assets/Scripts/SongPlayer.cs
+++ Assets/Scripts/SongPlayer.cs
@@ -7,11 +7,16 @@ public class SongPlayer : MonoBehaviour
 {
     /** Set in editor to the song number for this scene */
     public int songNumber;
+    [SerializeField] private MusicManager musicManager;
 
     void Start()
     {
         if (MusicManager.musicManager != null)
         {
+            MusicManager.musicManager.PlayMusic(songNumber);
+        } else
+        {
+            Instantiate(musicManager);
             MusicManager.musicManager.PlayMusic(songNumber);
         }
     }
