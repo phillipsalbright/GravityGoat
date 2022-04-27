8768b3f71a76cc0e71b97593b2fa4efbe104d89a
Phillips <phillipsalbright4@gmail.com>
2022-04-26 23:04:08 -0400

    Added levels, temp ending

--- Assets/Scripts/DeathBarrierScript.cs
+++ Assets/Scripts/DeathBarrierScript.cs
@@ -8,6 +8,19 @@ public class DeathBarrierScript : MonoBehaviour
         if (other.gameObject.layer == 3)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        } else if (other.gameObject.layer == 7 || other.gameObject.layer == 8)
+        {
+            Destroy(other.gameObject);
+            GravityGunScript gg = GameObject.FindObjectOfType<GravityGunScript>();
+            if (gg != null)
+            {
+                gg.orbCount++;
+            }
+            PlayerUIScript ui = GameObject.FindObjectOfType<PlayerUIScript>();
+            if (ui != null)
+            {
+                ui.UpdateOrbCount(gg.orbCount);
+            }
         }
     }
 }
